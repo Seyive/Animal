a2a687494b0e2cd27b95f3bc269b6d194afee050
seyive <sei4qurah@gmail.com>
2018-05-30 20:21:31 +0100

    Initial commit

---
+++ myScript.js
@@ -0,0 +1,15 @@
+<script type = "text/javascript">
+
+var animals = ['Sheep', 'Cat', 'Camel', 'Chicken', 'Chimpanzee']
+
+		let favouriteAnimal = prompt("Type in your favourite Animal");
+
+		alert ("Gotcha!" + " " + favouriteAnimal + " " + "Have got you covered");
+
+		if(animals.includes(favouriteAnimal));{
+
+		}else{
+
+			alert ("Ah ooooh!!!" + " " + favouriteAnimal + " " + "Favourite Animal not included.");
+		}
+</script>
